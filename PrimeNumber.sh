73b1d63bb63d7d56ca903b283f9f967daef10623
alekhya87 <davulurialekhya87909.com>
2020-10-09 23:33:06 +0530

    prime reverse order

--- PrimeNumber.sh
+++ PrimeNumber.sh
@@ -1,2 +1,44 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo "welcome to prime number "
+#print first 100 prime nos. in reverse order
+
+function initialization(){
+echo " enter a range"
+read range
+checkPrime $((range))
+}
+
+function checkPrime(){
+count=1
+counter=2
+range=$1
+while [ $count -le $range ]
+do
+	flag=0
+	for (( j=2; j<=$(($counter/2)); j++ ))
+	do
+		if [ $(($counter%$j)) -eq 0 ]
+		then
+			flag=1
+			break
+		fi
+	done
+	if [ $flag -eq 0 ]
+	then
+		#echo $counter
+		array[count]=$counter
+		((count++))
+	fi
+	((counter++))
+done
+arraySorting ${array[@]}
+}
+function arraySorting(){
+for (( k=${#array[@]}; k>=0; k-- ))
+do
+	
+	echo "${array[k]}"
+done
+}
+initialization
+
